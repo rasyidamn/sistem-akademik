c4058fb997445b9e74040b045107eb8f0ac0a60c
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-07-02 23:39:20 +0700

    baruu

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/sistem-akademik/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
