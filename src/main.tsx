64f4b00d300bb05755be8cd29a335c9d08d60ea0
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-07-03 01:04:35 +0700

    hehe

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {  createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import LoginPage from "./pages/LoginPage.tsx";
 import MainLayout from "./layout/MainLayout.tsx";
 
 const router = createBrowserRouter(
   [
     {
-      path: "/dashboard",
-      element: <MainLayout />
+      path: "/",
+      element: <Navigate to="/login" replace />
     },
     {
+      path: "/dashboard",
+      element: <MainLayout />,
+    },
+    {
+      index: true,
       path: "/login",
-      element: <LoginPage />
-    }
+      element: <LoginPage />,
+    },
   ],
   {
-    basename: "/sistem-akademik"
+    basename: "/sistem-akademik",
   }
-  
-)
-
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
