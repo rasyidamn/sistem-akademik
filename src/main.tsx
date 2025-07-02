7e863cc516397f33b322ae0be03349b9b0afab4d
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-07-03 00:21:37 +0700

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {   createHashRouter, RouterProvider } from "react-router";
+import {  createBrowserRouter, RouterProvider } from "react-router";
 import LoginPage from "./pages/LoginPage.tsx";
 import MainLayout from "./layout/MainLayout.tsx";
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   [
     {
       path: "/",
